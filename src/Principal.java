b6af5a05fca2fbba4f1e8f4445f1e1e4129ae191
Rafaela <rafaelaleite2017@icloud.com>
2025-11-26 11:21:49 -0300

    feat:toString

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,8 @@
-import br.com.alura.screenmatch.modelos.CalculadoraDeTempo;
-import br.com.alura.screenmatch.modelos.Filme;
-import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.Recomendacao;
+import br.com.alura.screenmatch.modelos.*;
+
+import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
@@ -35,6 +37,31 @@ public class Principal {
         calc.inclui(outroFilme);
         calc.inclui(serie);
         System.out.println(calc.getTempoTotal());
+
+        Recomendacao filtro = new Recomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(serie);
+        episodio.setTotalVisualizacao(300);
+        filtro.filtra(episodio);
+
+        var filmeDaRafa = new Filme();
+        filmeDaRafa.setNome("Dogville");
+        filmeDaRafa.setDuracaoEmMinutos(200);
+        filmeDaRafa.setAnoDeLancamento(2003);
+        filmeDaRafa.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDaRafa);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista " + listaDeFilmes.size());
+        System.out.println("Primeiro filme " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
+        System.out.println("toString do filme " + listaDeFilmes.get(0).toString());
+
 
 
 
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme extends Titulo {
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
    private String diretor;
 
     public String getDiretor() {
@@ -9,5 +11,15 @@ public class Filme extends Titulo {
 
     public void setDiretor(String diretor) {
         this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return (int)obterMedia() / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
     }
 }
